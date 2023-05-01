1c165d73cda19d6357f78076b0bc4294ffc0dd04
Brian Eng <brian@brianeng.com>
2023-05-01 15:51:33 +0000

    starter_app -> starter_app_with_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Resources
+  resources "sessions"
+  resources "users"
+  
+  # Login/out
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
 
-  # Defines the root path route ("/")
-  # get "/", :controller => "tacos", :action => "index"
+  # Define the root route
+  get "/", :controller => "users", :action => "new"
 end
