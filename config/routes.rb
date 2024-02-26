407a80db42bb2376ad40d3716078c3fe269d88cf
Benjamin Block <bjblock@gmail.com>
2024-02-26 22:53:49 +0000

    formats routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   resources "sessions"
   resources "users"
   
-  # Login/out
-  get "/login", :controller => "sessions", :action => "new"
-  get "/logout", :controller => "sessions", :action => "destroy"
+  # Login/logout
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
 
   # Define the root route
-  get "/", :controller => "users", :action => "new"
+  get("/", { :controller => "users", :action => "new" })
 end
