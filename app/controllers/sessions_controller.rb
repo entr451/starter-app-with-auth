ee005554518415350e23dd0a8dc02a2c438faa2c
Benjamin Block <bjblock@gmail.com>
2024-02-26 22:54:14 +0000

    makes nil guard explicit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     @user = User.find_by({ "username" => params["username"] })
-    if @user
+    if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "You are now logged-in."
